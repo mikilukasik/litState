937134d6835280a361304227d1fbf0438811f1be
mikilukasik <szarhazi@gmail.com>
2023-03-30 17:28:02 +0100

    shorter componentIds

--- src/getComponentIdFromStack.ts
+++ src/getComponentIdFromStack.ts
@@ -1,15 +1,28 @@
+const idCache: Record<string, string> = {};
+let nextId = 0;
+
 export const getComponentIdFromStack = () => {
   const { stack } = new Error();
 
-  return (
-    [2, 3]
-      .map(row =>
-        stack
-          ?.split('\n')
-          [row].split(' ')
-          .pop()
-          ?.replace(window.location.href, '')
-      )
-      .join('') || 'unknown-caller'
-  );
+  const longId = [2, 3]
+    .map(row =>
+      stack
+        ?.split('\n')
+        [row].split(' ')
+        .pop()
+        ?.replace(window.location.href, '')
+    )
+    .join('');
+
+  if (!longId) {
+    console.warn('Counld not generate component id');
+    return Math.random().toString().replace('0.', 'lsCrnd');
+  }
+
+  if (!idCache[longId]) {
+    idCache[longId] = `lsC${nextId}`;
+    nextId += 1;
+  }
+
+  return idCache[longId];
 };
